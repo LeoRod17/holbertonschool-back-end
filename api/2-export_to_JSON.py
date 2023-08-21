5e86e4474b3a56e244216d57ef8db600cd876b4a
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-21 14:48:49 -0700

    fixing pepo8

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Using what you did in the task #0, extend your
 Python script to export data in the JSON format.."""
-import requests
-from sys import argv
 import json
+from sys import argv
+import requests
+
 
 if __name__ == '__main__':
-    id = int(argv[1])
-    lista =[]
+    id = argv[1]
+    lista = []
     title = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
             id)).json()
@@ -20,5 +21,6 @@ if __name__ == '__main__':
     with open("{}.json".format(id), mode="w") as f:
         for x in title:
             lista.append({
-                "task:": x["title"], "completed": x["completed"], "username": name})
-        json.dump(lista,f)
+                "task:": x["title"], "completed": x["completed"],
+                "username": name})
+        json.dump(lista, f)
