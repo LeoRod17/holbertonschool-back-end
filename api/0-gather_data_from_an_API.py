55e4706ccee5cda63032b7095123f3637f73b0b3
root <matiassotelo171097@gmail.com>
2023-08-21 07:40:46 -0700

    task 0 v2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """a Python script that, using this REST API,
-for a given employee ID, returns information about his/her TODO
+for a given employee ID, returns information about his/her todo
 list progress."""
 import requests
 from sys import argv
 
-
-id = argv[1]
-req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+if __name__ == '__main__':
+    id = argv[1]
+    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                    .format(id)).json()
-for x in req:
-    print(x)
+    for x in req:
+        print(x)
