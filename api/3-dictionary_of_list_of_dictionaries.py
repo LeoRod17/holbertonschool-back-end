0003a34cea30e7d3f0afc7c3d68f749f8f674ce0
root <matiassotelo171097@gmail.com>
2023-08-22 07:24:54 -0700

    task 3 v1

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your
+Python script to export data in the JSON format.."""
+import json
+import requests
+
+
+if __name__ == '__main__':
+    lista = []
+    dic = {}
+    title = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    usu = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+    
+    with open("todo_all_employees.json", mode="w") as f:
+        for y in usu:
+            user = int(y["id"])
+            for x in title:
+                if x["id"] == user:
+                    lista.append({
+                        "username": y["username"],
+                        "task": x['title'],
+                        "completed": x["completed"]
+                    })
+            dic[str(user)] = lista
+            lista = []
+        json.dump(dic, f)
